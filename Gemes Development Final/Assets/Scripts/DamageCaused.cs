823e4f7d369e4902a4c28f485a6078730dee5a1a
Carlos kb2 <kb2studio@gmail.com>
2024-01-24 11:11:07 -0500

    Updated respawn logic to use an array to pick point from, the array number is updated by the enemy that caused the damage

--- Gemes Development Final/Assets/Scripts/DamageCaused.cs
+++ Gemes Development Final/Assets/Scripts/DamageCaused.cs
@@ -4,13 +4,15 @@ public class DamageCaused : MonoBehaviour
 {
     public PlayerHealth playerHealth;
     public int damage = 10;
+    public int spawnPointIndex = 0; // Index of the spawn point to set when this enemy damages the player
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            print("taking damage" + playerHealth.health);
+            PlayerSpawn.Instance.SetSpawnPoint(spawnPointIndex); // Update spawn point first
             playerHealth.TakeDamage(damage);
         }
     }
+
 }
--- Gemes Development Final/Assets/Scripts/PlayerSpawn.cs
+++ Gemes Development Final/Assets/Scripts/PlayerSpawn.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
+using System.Collections.Generic; // Needed for lists
 
 public class PlayerSpawn : MonoBehaviour
 {
-    [SerializeField]
-    GameObject playerPrefab;
-    [SerializeField]
-    Transform spawnPoint;
+    public GameObject playerPrefab;
+    public List<Transform> spawnPoints; // List of spawn points
     public static PlayerSpawn Instance { get; private set; }
 
     private GameObject currentPlayerInstance;
+    private int currentSpawnPointIndex = 0; // Index of the current spawn point
 
     private void Awake()
     {
@@ -19,7 +19,21 @@ public class PlayerSpawn : MonoBehaviour
         else
         {
             Instance = this;
-            SpawnPlayer(); // Instantiate the player when the game starts
+            SpawnPlayer(); // Instantiate the player at the start
+        }
+    }
+
+    // Call this method to update the current spawn point
+    public void SetSpawnPoint(int spawnPointIndex)
+    {
+        if (spawnPointIndex >= 0 && spawnPointIndex < spawnPoints.Count)
+        {
+            currentSpawnPointIndex = spawnPointIndex;
+            Debug.Log("Setting spawn point index to: " + spawnPointIndex);
+        }
+        else
+        {
+            Debug.LogError("Invalid spawn point index.");
         }
     }
 
@@ -30,9 +44,13 @@ public class PlayerSpawn : MonoBehaviour
             Destroy(currentPlayerInstance);
         }
 
-        if (playerPrefab != null && spawnPoint != null)
+        if (playerPrefab != null && spawnPoints.Count > 0)
         {
+            Transform spawnPoint = spawnPoints[currentSpawnPointIndex];
             currentPlayerInstance = Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+            Debug.Log("Spawning player at index: " + currentSpawnPointIndex);
         }
     }
+
+
 }
