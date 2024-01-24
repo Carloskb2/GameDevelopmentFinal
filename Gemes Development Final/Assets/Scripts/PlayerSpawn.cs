7c811aa679eb720556ad5adad66e75036322370e
Carlos kb2 <kb2studio@gmail.com>
2024-01-24 09:10:37 -0500

    Added Player Prefab and PlayerSpawn Script

---
+++ Gemes Development Final/Assets/Scripts/PlayerSpawn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerPrefab;
+    [SerializeField]
+    private Transform spawnPoint;
+
+    void Start()
+    {
+        SpawnPlayer();
+    }
+
+    void SpawnPlayer()
+    {
+        if (playerPrefab != null && spawnPoint != null)
+        {
+            Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+}
+
