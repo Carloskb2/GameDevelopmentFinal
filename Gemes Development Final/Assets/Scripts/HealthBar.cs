fd60b038495e2372a3f882439432d40cf5737bb4
Carlos kb2 <kb2studio@gmail.com>
2024-01-29 11:49:07 -0500

    Added HealthBar UI to Canvas in CarlosTestScene

---
+++ Gemes Development Final/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public PlayerHealth playerHealth; // Reference to the PlayerHealth ScriptableObject
+    public Image healthBarImage; // Reference to the health bar Image
+
+    void Update()
+    {
+        // Update health bar based on player's health
+        float healthPercentage = (float)playerHealth.health / 100.0f;
+        healthBarImage.fillAmount = healthPercentage;
+    }
+}
