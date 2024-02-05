ccac71a317926d267b44acff65dfe25d25886370
Carlos kb2 <kb2studio@gmail.com>
2024-02-05 15:56:40 -0500

    Added Game manager and exit portal that loads next level to CarlosTestScene // both have prefabs to be used on the other scenes

---
+++ Gemes Development Final/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    
+    [Header("Level name")]
+    public string currentLevel;
+    public string nextLevel;
+
+    public void LoadNextLevel()
+    {
+        currentLevel = nextLevel; // Update current level to the next one
+        SceneManager.LoadScene(nextLevel); // Load the next level
+    }
+}
