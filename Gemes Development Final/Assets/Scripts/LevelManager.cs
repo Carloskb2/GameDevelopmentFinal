1326a53d064398d8564c7ff5a8d4a29dd6995b0d
Carlos kb2 <kb2studio@gmail.com>
2024-02-06 12:06:23 -0500

    Updated Level manager // Now an amount of collectables should be collected before the exit portal appears

--- Gemes Development Final/Assets/Scripts/LevelManager.cs
+++ Gemes Development Final/Assets/Scripts/LevelManager.cs
@@ -3,14 +3,45 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    
-    [Header("Level name")]
+    [Header("Level Information")]
     public string currentLevel;
     public string nextLevel;
 
+    [Header("Collectable Management")]
+    public int collectablesNeeded = 5;
+    [SerializeField]
+    private int collectablesCollected = 0;
+    [SerializeField]
+    private GameObject portal;
+
+    void Start()
+    {
+        if (portal != null) portal.SetActive(false);
+        Collectable.OnCollected += CollectableCollected; // Subscribe to the event
+    }
+
+    void OnDestroy()
+    {
+        Collectable.OnCollected -= CollectableCollected; // Unsubscribe to prevent memory leaks
+    }
+
     public void LoadNextLevel()
     {
-        currentLevel = nextLevel; // Update current level to the next one
-        SceneManager.LoadScene(nextLevel); // Load the next level
+        currentLevel = nextLevel;
+        SceneManager.LoadScene(nextLevel);
+    }
+
+    private void CollectableCollected(CollectableID id)
+    {
+        collectablesCollected++;
+        CheckCollectables();
+    }
+
+    void CheckCollectables()
+    {
+        if (collectablesCollected >= collectablesNeeded && portal != null)
+        {
+            portal.SetActive(true); // Activate the portal
+        }
     }
 }
