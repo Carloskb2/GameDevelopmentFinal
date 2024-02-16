d4ca859253aac6ee5a4f4d5295d9dbba0574a4d6
eleceb <cheungcaleb1030@gmail.com>
2024-02-16 21:38:36 +0800

    Finished Level1, Level2, and Level3

---
+++ Gemes Development Final/Assets/Caleb/Scripts/TriggerNewSpawnPoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class TriggerNewSpawnPoint : MonoBehaviour
+{
+    [SerializeField] int spawnPointIndex;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            PlayerSpawn.Instance.SetSpawnPoint(spawnPointIndex);
+        }
+    }
+}
