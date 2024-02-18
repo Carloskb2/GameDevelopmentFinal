382aa0d08cc238e0cf44c2261b2129a9683d7b76
eleceb <cheungcaleb1030@gmail.com>
2024-02-19 01:36:47 +0800

    Added scripts for lock and unlock levels in Level Selection Screen

---
+++ Gemes Development Final/Assets/Caleb/Scripts/ProgressManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressManager : MonoBehaviour
+{
+    public Button[] levelButtons;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        int highestLevelReached = PlayerPrefs.GetInt("HighestLevelReached", 1);
+
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            if (i + 1 <= highestLevelReached)
+            {
+                levelButtons[i].interactable = true;
+            }
+            else
+            {
+                levelButtons[i].interactable = false;
+            }
+        }
+    }
+}
