2e74c4be4134355de5bee62a0742e5b2a8265b5b
eleceb <cheungcaleb1030@gmail.com>
2024-02-21 19:22:01 +0800

    Added Audio for the player

---
+++ Gemes Development Final/Assets/Caleb/Scripts/PlayAudioFromAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioFromAnimation : MonoBehaviour
+{
+    AudioSource sfxSource;
+    Animator animator;
+
+    private void Awake()
+    {
+        sfxSource = gameObject.GetComponent<AudioSource>();
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+
+    private void LateUpdate()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle") || !animator.enabled)
+        {
+            sfxSource.Stop();
+        }else if (animator.GetCurrentAnimatorStateInfo(0).IsName("Fall"))
+        {
+
+        }
+
+    }
+}
