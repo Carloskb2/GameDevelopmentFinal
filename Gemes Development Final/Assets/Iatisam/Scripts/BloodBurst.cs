f480fa61fe880b3d6942a8ab7f3ef4f6432dc2e5
Iatisamnawaz <Iatisamnawaz@users.noreply.github.com>
2024-02-24 02:17:11 +0500

    Added bleeding effect to Level 6

---
+++ Gemes Development Final/Assets/Iatisam/Scripts/BloodBurst.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class BloodBurst : MonoBehaviour
+{
+    public GameObject explosionEffect;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        { 
+            ExplodeEffect();
+        }
+    }
+
+     private void ExplodeEffect()
+    {
+        Debug.Log("BloodBurst");
+        GameObject explosion = Instantiate(explosionEffect, transform.position, Quaternion.identity);
+        Destroy(explosion, 0.1f);
+
+    }
+
+}
