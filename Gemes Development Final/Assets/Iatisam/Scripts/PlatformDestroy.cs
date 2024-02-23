69742e2639efa089e1901f3b1778e6eb68d74206
Iatisamnawaz <Iatisamnawaz@users.noreply.github.com>
2024-02-24 00:09:51 +0500

    created explosive platforms

---
+++ Gemes Development Final/Assets/Iatisam/Scripts/PlatformDestroy.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class PlatformDestroy : MonoBehaviour
+{
+    private bool platformDestroyed = false;
+    private Vector3 originalPosition;
+    public GameObject platform;
+    public GameObject explosionEffect;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Invoke("DestroyPlatform", 0.5f);
+
+        }
+    }
+
+    private void DestroyPlatform()
+    {
+        
+        Debug.Log("Destroying Platform");
+
+        platformDestroyed = true;
+        ExplodeEffect();
+        gameObject.SetActive(false);
+
+
+        Invoke("PlatformRestore", 1.0f);
+
+
+    }
+
+    private void PlatformRestore()
+    {
+        Debug.Log("Respawning Platform");
+
+        if (platformDestroyed && platform != null)
+        {
+            gameObject.SetActive(true);
+
+        }
+    }
+
+     private void ExplodeEffect()
+    {
+        // Instantiate the explosion prefab at the platform's position
+        GameObject explosion = Instantiate(explosionEffect, transform.position, Quaternion.identity);
+        Destroy(explosion, 0.1f);
+
+    }
+
+}
