0e08ba673c68db770b5c1cb289b3337f123e40b1
Iatisamnawaz <Iatisamnawaz@users.noreply.github.com>
2024-02-23 18:10:16 +0500

    Added Game Win Screen / Script

---
+++ Gemes Development Final/Assets/Iatisam/Scripts/GameWinScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameWinScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("GameWin");
+
+        }
+    }
+}
