c7dd48fe6d0e97b31b9fabfa1b08e8065b5657df
eleceb <cheungcaleb1030@gmail.com>
2024-02-29 23:02:34 +0800

    Improved the PlatformDestroy script

--- Gemes Development Final/Assets/Iatisam/Scripts/PlatformDestroy.cs
+++ Gemes Development Final/Assets/Iatisam/Scripts/PlatformDestroy.cs
@@ -12,25 +12,24 @@ public class PlatformDestroy : MonoBehaviour
         if (collision.collider.CompareTag("Player"))
         {
             Invoke("DestroyPlatform", 0.5f);
-
         }
     }
 
     private void DestroyPlatform()
     {
-        
         Debug.Log("Destroying Platform");
 
-        platformDestroyed = true;
-        AudioManager.Instance.PlaySound(AudioManager.Instance.collapseSound);
+        if (!platformDestroyed)
+        {
+            gameObject.SetActive(false);
 
-        ExplodeEffect();
-        gameObject.SetActive(false);
+            ExplodeEffect();
+            AudioManager.Instance.PlaySound(AudioManager.Instance.collapseSound);
 
+            Invoke("PlatformRestore", 1.0f);
 
-        Invoke("PlatformRestore", 1.0f);
-
-
+            platformDestroyed = true;
+        }
     }
 
     private void PlatformRestore()
@@ -40,16 +39,15 @@ public class PlatformDestroy : MonoBehaviour
         if (platformDestroyed && platform != null)
         {
             gameObject.SetActive(true);
-
+            platformDestroyed = false;
         }
     }
 
      private void ExplodeEffect()
     {
         // Instantiate the explosion prefab at the platform's position
-        GameObject explosion = Instantiate(explosionEffect, transform.position, Quaternion.identity);
+        GameObject explosion = Instantiate(explosionEffect, new Vector3(transform.position.x, transform.position.y, transform.position.z - 5), Quaternion.identity);
         Destroy(explosion, 0.1f);
-
     }
 
 }
