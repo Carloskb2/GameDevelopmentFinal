e19b3c983d8b00abd43576ce8fbdc12ec925d64e
eleceb <cheungcaleb1030@gmail.com>
2024-03-01 00:58:01 +0800

    Fixed the another bug of the audio cross scene

--- Gemes Development Final/Assets/Scripts/AudioManager.cs
+++ Gemes Development Final/Assets/Scripts/AudioManager.cs
@@ -44,7 +44,6 @@ public class AudioManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
