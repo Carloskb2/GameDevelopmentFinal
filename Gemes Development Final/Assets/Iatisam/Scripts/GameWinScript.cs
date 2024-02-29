1a4c890fe320a8e0b427ecbd0cd8e0c1c9923a1f
eleceb <cheungcaleb1030@gmail.com>
2024-03-01 03:12:15 +0800

    Fixed the few bugs in Level 5

--- Gemes Development Final/Assets/Iatisam/Scripts/GameWinScript.cs
+++ Gemes Development Final/Assets/Iatisam/Scripts/GameWinScript.cs
@@ -1,9 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameWinScript : MonoBehaviour
 {
     void Start()
     {
         AudioManager.Instance.PlaySound(AudioManager.Instance.winSound);
+    }
+
+    // Loads the main menu scene
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f; // Ensure the game's time scale is reset before loading the main menu
+        SceneManager.LoadScene("Start Menu"); // Load the main menu scene
+    }
+
+    // Quits the game
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit(); // Quit the application
     }
 }
